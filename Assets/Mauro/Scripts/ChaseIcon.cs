11da5609ef1be7ef0dfb0f095cdd7db2dca82bf6
Mauro Vanetti <posta@maurovanetti.info>
2026-01-31 13:38:01 +0100

    Smooth chase

--- Assets/Mauro/Scripts/ChaseIcon.cs
+++ Assets/Mauro/Scripts/ChaseIcon.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class ChaseIcon : MonoBehaviour
 {
-    public float progress = 0.0f; // 0 to 1
+    public float progressMaxSpeed = 0.2f;
+    private float targetProgress = 999; // 0 to 1
+    private float actualProgress = 999; // 0 to 1
     private RectTransform rectTransform;
+    private float speed = 0f;
+
+    public float Progress { set { targetProgress = value; } }
+    public float Speed { set { speed = value; } }
+    public float ActualProgress => actualProgress;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,11 +21,18 @@ public class ChaseIcon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Calculate parent's width (UI panel)
+        targetProgress += speed * Time.deltaTime;
         float parentWidth = transform.parent.GetComponent<RectTransform>().rect.width;
         float ownWidth = rectTransform.rect.width;
-        // Set local position based on progress
-        float inset = (parentWidth - ownWidth) * progress;
+        if (targetProgress < actualProgress)
+        {
+            actualProgress = targetProgress;
+        }
+        else
+        {
+            actualProgress = Mathf.MoveTowards(actualProgress, targetProgress, progressMaxSpeed * Time.deltaTime);
+        }
+        float inset = (parentWidth - ownWidth) * actualProgress;
         rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, inset, rectTransform.rect.width);
     }
 }
