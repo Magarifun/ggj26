6d6d5da8b5c9d525f075d133687c8fa4879e0fdc
Alessandro Elifani <alessandro.elifani@icloud.com>
2026-01-31 20:28:35 +0100

    Unlocked Behaviour

--- Assets/Scenes/_Ale/Scripts/TileFxListener.cs
+++ Assets/Scenes/_Ale/Scripts/TileFxListener.cs
@@ -25,24 +25,24 @@ public class TileFxListener : MonoBehaviour
     private void HoverEnter(CardDragAndDrop2D_SnapSortingErase card, Collider2D tile)
     {
         // esempio highlight
-        var sr = tile.GetComponent<SpriteRenderer>();
-        if (sr != null) sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 1f);
+       Debug.Log("Hover Enter: " + card.name);
     }
 
     private void HoverExit(CardDragAndDrop2D_SnapSortingErase card, Collider2D tile)
     {
         // esempio unhighlight (qui fai come vuoi)
+        Debug.Log("Hover Exit: " + card.name);
     }
 
     private void Grabbed(CardDragAndDrop2D_SnapSortingErase card)
     {
         // esempio suono / fx
-        // Debug.Log("Grabbed: " + card.name);
+         Debug.Log("Grabbed: " + card.name);
     }
 
     private void Placed(CardDragAndDrop2D_SnapSortingErase card, BoardSlot2D slot)
     {
         // esempio: quando piazzata
-        // Debug.Log($"Placed {card.name} on slot {(slot ? slot.name : "null")}");
+         Debug.Log($"Placed {card.name} on slot {(slot ? slot.name : "null")}");
     }
 }
